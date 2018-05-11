036b9f836fbde9a4d74968e5f313ac0189e3f9e5
Terry Yin <terry.yinzhe@gmail.com>
2018-05-11 15:46:07 +0800

    polish

--- app/controllers/wiki/wiki_page_controller.rb
+++ app/controllers/wiki/wiki_page_controller.rb
@@ -1,45 +1,45 @@
 # frozen_string_literal: true
 
 module Wiki
-class WikiPageController < Wiki::ApplicationController
-  layout 'wiki'
-  before_action :set_wiki_space
-  before_action :set_object, only: %i[show edit update]
-  respond_to :html
+  class WikiPageController < Wiki::ApplicationController
+    layout 'wiki'
+    before_action :set_wiki_space
+    before_action :set_object, only: %i[show edit update]
+    respond_to :html
 
-  def show
-    @title = @wiki_page.title
-    render 'new' unless @wiki_page.id?
+    def show
+      @title = @wiki_page.title
+      render 'new' unless @wiki_page.id?
+    end
+
+    def edit; end
+
+    def create
+      @wiki_page = @wiki_space.wiki_pages.create(wiki_page_params.merge(user: current_user))
+      respond_with(@wiki_space, @wiki_page)
+    end
+
+    def update
+      @wiki_page.update(wiki_page_params.merge(user: current_user))
+      respond_with(@wiki_space, @wiki_page)
+    end
+
+    private
+
+    def path
+      URI.decode params[:path]
+    end
+
+    def set_wiki_space
+      @wiki_space = WikiSpace.find_by!(title: params[:wiki_space_id])
+    end
+
+    def set_object
+      @wiki_page = @wiki_space.wiki_pages.find_or_initialize_by(path: path)
+    end
+
+    def wiki_page_params
+      params.require(:wiki_page).permit(:path, :body, :title)
+    end
   end
-
-  def edit; end
-
-  def create
-    @wiki_page = @wiki_space.wiki_pages.create(wiki_page_params.merge(user: current_user))
-    respond_with(@wiki_space, @wiki_page)
-  end
-
-  def update
-    @wiki_page.update(wiki_page_params.merge(user: current_user))
-    respond_with(@wiki_space, @wiki_page)
-  end
-
-  private
-
-  def path
-    URI.decode params[:path]
-  end
-
-  def set_wiki_space
-    @wiki_space = WikiSpace.find_by!(title: params[:wiki_space_id])
-  end
-
-  def set_object
-    @wiki_page = @wiki_space.wiki_pages.find_or_initialize_by(path: path)
-  end
-
-  def wiki_page_params
-    params.require(:wiki_page).permit(:path, :body, :title)
-  end
-end
 end
--- spec/wiki/wiki_controller_spec.rb
+++ spec/wiki/wiki_controller_spec.rb
@@ -3,60 +3,60 @@
 require 'rails_helper'
 
 module Wiki
-RSpec.describe WikiPageController, type: :controller do
-  routes { Engine.routes }
-  let(:page) { create :wiki_page }
-  let(:space) { page.wiki_space }
+  RSpec.describe WikiPageController, type: :controller do
+    routes { Engine.routes }
+    let(:page) { create :wiki_page }
+    let(:space) { page.wiki_space }
 
-  describe 'Wiki show' do
-    describe 'show' do
-      it 'render the layout' do
-        get :show, params: { wiki_space_id: space.to_param, path: page.to_param }
-        expect(response).to render_template('show', layout: 'wiki')
+    describe 'Wiki show' do
+      describe 'show' do
+        it 'render the layout' do
+          get :show, params: { wiki_space_id: space.to_param, path: page.to_param }
+          expect(response).to render_template('show', layout: 'wiki')
+          expect(assigns(:wiki_page)).to eq page
+          expect(assigns(:title)).to eq 'a wiki'
+        end
+
+        it 'creating non-existing page' do
+          get :show, params: { wiki_space_id: space.to_param, path: 'new/id' }
+          expect(assigns(:wiki_page)).to be_a_new(WikiPage)
+          expect(assigns(:wiki_page).path).to eq 'new/id'
+          expect(response).to render_template('new')
+        end
+
+        it 'convert path to normal text' do
+          get :show, params: { wiki_space_id: space.to_param, path: 'This%20is%20a%20pen' }
+          expect(assigns(:wiki_page).path).to eq 'This is a pen'
+        end
+      end
+    end
+
+    describe 'Wiki edit' do
+      it 'find the page' do
+        get :edit, params: { wiki_space_id: space.to_param, path: page.to_param }
         expect(assigns(:wiki_page)).to eq page
-        expect(assigns(:title)).to eq 'a wiki'
+      end
+    end
+
+    describe 'Wiki update and create' do
+      let(:user) { create :user }
+      before { allow(controller).to receive(:current_user) { user } }
+
+      it 'upate the page' do
+        patch :update, params: { wiki_space_id: space.to_param, path: page.to_param, wiki_page: { body: 'blah', title: 'new name' } }
+        expect(response).to redirect_to([space, page])
+        page.reload
+        expect(page.title).to eq 'new name'
+        expect(page.current_revision.user).to eq user
       end
 
-      it 'creating non-existing page' do
-        get :show, params: { wiki_space_id: space.to_param, path: 'new/id' }
-        expect(assigns(:wiki_page)).to be_a_new(WikiPage)
-        expect(assigns(:wiki_page).path).to eq 'new/id'
-        expect(response).to render_template('new')
-      end
-
-      it 'convert path to normal text' do
-        get :show, params: { wiki_space_id: space.to_param, path: 'This%20is%20a%20pen' }
-        expect(assigns(:wiki_page).path).to eq 'This is a pen'
+      it 'create the page' do
+        post :create, params: { wiki_space_id: space.to_param, wiki_page: { path: 'path', body: 'blah', title: 'new name' } }
+        expect(assigns(:wiki_page)).to be_a(WikiPage)
+        expect(assigns(:wiki_page)).to be_persisted
+        expect(assigns(:wiki_page).current_revision.user).to eq user
+        expect(response).to redirect_to([space, assigns(:wiki_page)])
       end
     end
   end
-
-  describe 'Wiki edit' do
-    it 'find the page' do
-      get :edit, params: { wiki_space_id: space.to_param, path: page.to_param }
-      expect(assigns(:wiki_page)).to eq page
-    end
-  end
-
-  describe 'Wiki update and create' do
-    let(:user) { create :user }
-    before { allow(controller).to receive(:current_user) { user } }
-
-    it 'upate the page' do
-      patch :update, params: { wiki_space_id: space.to_param, path: page.to_param, wiki_page: { body: 'blah', title: 'new name' } }
-      expect(response).to redirect_to([space, page])
-      page.reload
-      expect(page.title).to eq 'new name'
-      expect(page.current_revision.user).to eq user
-    end
-
-    it 'create the page' do
-      post :create, params: { wiki_space_id: space.to_param, wiki_page: { path: 'path', body: 'blah', title: 'new name' } }
-      expect(assigns(:wiki_page)).to be_a(WikiPage)
-      expect(assigns(:wiki_page)).to be_persisted
-      expect(assigns(:wiki_page).current_revision.user).to eq user
-      expect(response).to redirect_to([space, assigns(:wiki_page)])
-    end
-  end
-end
 end
